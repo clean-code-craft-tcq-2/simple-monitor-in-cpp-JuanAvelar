b1203e040fca8661864f77851996510bfa6488bc
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-02-11 00:51:02 -0600

    Just converted private functions into public to test, not sure if that is valid though

--- checker.cpp
+++ checker.cpp
@@ -12,6 +12,7 @@ private:
   const float _MinTemp, _MaxTemp;
   const float _MinSoC, _MaxSoC;
   const float _MaxChargeRate;
+public:
   bool IsTemperatureDanger(float temp){
     bool ret = (temp < _MinTemp || temp > _MaxTemp);
     if(ret) std::cerr << "Temperature out of range!\n";
@@ -27,7 +28,6 @@ private:
     if(ret) std::cerr << "Charge Rate out of range!\n";
     return ret;
   }
-public:
   Battery(float MinTemp, float MaxTemp, float MinSoC, float MaxSoC, float MaxChargeRate):
           _MinTemp(MinTemp), _MaxTemp(MaxTemp), _MinSoC(MinSoC), _MaxSoC(MaxSoC), _MaxChargeRate(MaxChargeRate){};
   bool batteryIsOk(float temperature, float soc, float chargeRate);
@@ -43,12 +43,12 @@ int main() {
   //be used to test instead of a 3D space
   Battery MyBattery(0.0, 45.0, 20.0, 80.0, 0.8);
   for(float Temperature = 0; Temperature < 100; Temperature+=10){
-     assert(MyBattery.batteryIsOk(Temperature, 70, 0.7) == (0.0 <= Temperature && Temperature <= 45.0));
+     assert(MyBattery.batteryIsOk(Temperature, 70, 0.7) == !MyBattery.IsTemperatureDanger(Temperature));
   }
   for(float SoC = 0; SoC < 100; SoC+=10){
-     assert(MyBattery.batteryIsOk(40.0, SoC, 0.7) == (20.0 <= SoC && SoC <= 80.0));
+     assert(MyBattery.batteryIsOk(40.0, SoC, 0.7) == !MyBattery.IsStateOfChargeDanger(SoC));
   }
   for(float ChargeRate = 0; ChargeRate < 1.0; ChargeRate+=0.1){
-     assert(MyBattery.batteryIsOk(40.0, 70, ChargeRate) == (ChargeRate <= 0.8));
+     assert(MyBattery.batteryIsOk(40.0, 70, ChargeRate) == !MyBattery.IsChargeRateDanger(ChargeRate));
   }
 }
