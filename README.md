6895279c1e8b1f9b511583e56e49e7b24dca2502
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-02-17 15:43:59 -0600

    Early Warnings Added

--- README.md
+++ README.md
@@ -57,3 +57,7 @@ Shorten the Semantic distance
 - Functional to express relation between input and output
 - Object oriented to encapsulate state with actions
 - Apect oriented to capture repeating aspects
+
+##Extensions Added
+
+- Early Warnings with fixed 5% tolerance
--- checker.cpp
+++ checker.cpp
@@ -8,11 +8,17 @@ class Data_Limit{
 private:
   const float _Minimum_limit, _Maximum_limit;
   std::string _WarningMessage;
+  bool _EarlyWarningON;
+  bool EarlyWarning(float reading, float percentTolerance){
+    return (  ((reading < (_Minimum_limit + _Maximum_limit*percentTolerance)) &(reading >=_Minimum_limit))
+            | ((reading <= _Maximum_limit)                                    &(reading > _Maximum_limit*(1.0-percentTolerance))));
+  }
 public:
-  Data_Limit(float min, float max, std::string warning): _Minimum_limit(min), _Maximum_limit(max), _WarningMessage(warning){};
-  bool IsLimitDanger(float temp){
-    bool ret = (temp < _Minimum_limit || temp > _Maximum_limit);
-    if(ret) std::cerr << "WARNING: " << _WarningMessage << "\n";
+  Data_Limit(float min, float max, std::string warning, bool early_warning = false): _Minimum_limit(min), _Maximum_limit(max), _WarningMessage(warning), _EarlyWarningON(early_warning){};
+  bool IsLimitDanger(float reading){
+    bool ret = (reading < _Minimum_limit || reading > _Maximum_limit);
+    if(ret) std::cerr << "WARNING: " << _WarningMessage << " out of range!\n";
+    if(!ret & EarlyWarning(reading, 0.05) & _EarlyWarningON) std::cout << "IMPORTANT INFO: " << _WarningMessage << " approaching Limit\n";
     return ret;
   }
 };
@@ -35,18 +41,18 @@ int main() {
   //As each measurement is independent of each other a linear vector can
   //be used to test this instead of a 3D space
   std::vector<Data_Limit> Specification;
-  Specification.push_back(Data_Limit(00.0,45.0,"Temperature out of range!"));
-  Specification.push_back(Data_Limit(20.0,80.0,"State of Charge out of range!"));
-  Specification.push_back(Data_Limit(0.00,0.80,"Charge Rate out of range!"));
+  Specification.push_back(Data_Limit(00.0,45.0,"Temperature"));
+  Specification.push_back(Data_Limit(20.0,80.0,"State of Charge", true));//by default early warnings are turned OFF unless specified by parameter
+  Specification.push_back(Data_Limit(0.00,0.80,"Charge Rate"));
   Battery MyBattery(Specification);
-  for(float BaseIndex = 0; BaseIndex < 100; BaseIndex+=10){
+  for(float BaseIndex = 29.01; BaseIndex < 100; BaseIndex+=10){
     std::vector<float> SensorMapTest;
     std::cout << "\tTest index : " << BaseIndex << std::endl;
     SensorMapTest = {BaseIndex*(float)TEMPSCALARTEST, 70, 0.7};
-    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(1).IsLimitDanger(BaseIndex*TEMPSCALARTEST));
+    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(0).IsLimitDanger(BaseIndex*TEMPSCALARTEST));
     SensorMapTest = {40.0, BaseIndex*(float)SOCSCALARTEST, 0.7};
-    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(2).IsLimitDanger(BaseIndex*SOCSCALARTEST));
+    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(1).IsLimitDanger(BaseIndex*SOCSCALARTEST));
     SensorMapTest = {40.0, 70, BaseIndex*(float)CHRASCALARTEST};
-    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(3).IsLimitDanger(BaseIndex*CHRASCALARTEST));
+    assert(MyBattery.batteryIsOk(SensorMapTest) == !Specification.at(2).IsLimitDanger(BaseIndex*CHRASCALARTEST));
   }
 }
